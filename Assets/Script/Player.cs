fa89c867c4b83adfd713d7f90046218be1d1e3ce
APBorK <1659732@gmail.com>
2021-02-06 19:23:34 +0300

    gitigno

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Player : MonoBehaviour
+{
+    public LayerMask whatCanBeClickedOn;
+    private NavMeshAgent myAgents;
+    void Start()
+    {
+        myAgents = GetComponent<NavMeshAgent>();
+    }
+
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray myRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            if (Physics.Raycast(myRay,out hitInfo,100,whatCanBeClickedOn))
+            {
+                myAgents.SetDestination(hitInfo.point);
+            }
+        }
+    }
+}
